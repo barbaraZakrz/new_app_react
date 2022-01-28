05555dac68b85f918a7ecc2c23116044068abb5e
Basia <barbara.zakrz@gmail.com>
2022-01-28 23:31:56 +0100

    states

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,9 +2,14 @@ import React, { Component } from 'react';
 
 
 class Footer extends Component{
+    
+    state = {
+        name: 'Me'
+    }
 
-    changed(){
-        console.log("Changed")
+    changed = (evt) => {
+        this.setState({name: evt.target.value})
+        console.log(this.state.name)
     }
   
     render() { 
@@ -13,7 +18,8 @@ class Footer extends Component{
             <div>
                 <h2 onClick={this.props.myalert}> 
                     {this.props.trademark} </h2>
-                <input onChange={this.changed} type="text" />
+                <input value={this.state.name}
+                onChange={this.changed} type="text" />
             </div>
         )
     }
